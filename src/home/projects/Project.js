51fa714ebafad0d722028bdddfdbe1c00e84ca88
Nooxunath <mateksonic@gmail.com>
2020-12-02 18:23:01 +0100

    placement des position absolute pour les projets

--- src/home/projects/Project.js
+++ src/home/projects/Project.js
@@ -6,7 +6,8 @@ export class Project extends Component {
     render() {
         return (
             <Fragment>
-                <div className="slide-project"></div>
+                <div className="slide-project-1"></div>
+                <div className="slide-project-2"></div>
                 <section className="projet rainyDay">
                     <div className="col-preview">
                         <h3>RainyDay</h3>
@@ -14,7 +15,7 @@ export class Project extends Component {
                         <div className="card-project"></div>
                     </div>
                     <div id="#" className="col-svg">
-                        <img src={ready} alt="" className="ready"/>
+                        <img src={ready} alt="" className="svg-project"/>
                     </div>
                 </section>                
             </Fragment>
