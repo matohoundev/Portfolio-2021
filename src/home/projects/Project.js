4e84b83b594cfbe85fa3f5264854f7d8e9dcd5e0
Nooxunath <mateksonic@gmail.com>
2020-12-03 18:28:35 +0100

    compo principal fini en mobile -> demarrer responsivité tablette

--- src/home/projects/Project.js
+++ src/home/projects/Project.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ready from '../../assets/svg-projects/ready.svg';
+import readyTwo from '../../assets/svg-projects/readyTwo.svg';
 import './projects.scss';
 
 export class Project extends Component {
@@ -11,11 +12,12 @@ export class Project extends Component {
                 <section className="projet rainyDay">
                     <div className="col-preview">
                         <h3 className="h3-rainy">Rainy<b className="b-rainy">Day</b></h3>
-                        <em>React.js - Bootstrap - Javascri<mark className="mark-techno">pt - Firebase</mark></em>
-                        <div className="card-project"></div>
+                        <em>React.js - Bootstrap - Javascript -<mark className="mark-techno"> Firebase</mark></em>
+                        <div className="card-project parallax"></div>
                     </div>
                     <div id="#" className="col-mb">
                         <img src={ready} alt="" className="svg-project"/>
+                        <img src={readyTwo} alt="" className="svg-project-2"/>
                     </div>
                 </section>                
             </Fragment>
