52a3f5f1d97501810f41e92425a9ba1c4657899d
Nooxunath <mateksonic@gmail.com>
2020-12-14 17:57:22 +0100

    deboublement fait personnalisation des projects en cours

--- src/home/Main.js
+++ src/home/Main.js
@@ -1,5 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import Project from './projects/Project';
+import ready from '../assets/svg-projects/ready.svg';
+import readyTwo from '../assets/svg-projects/readyTwo.svg';
+import sephi from '../assets/svg-projects/sephi.svg';
+import epaule from '../assets/svg-projects/epaule.svg';
+import cloud from '../assets/svg-projects/cloud.svg';
+import marvin from '../assets/svg-projects/marvin.svg';
+import cuisto from '../assets/svg-projects/cuisto.svg';
 import './main.scss';
 
 export class Main extends Component {
@@ -8,44 +15,59 @@ export class Main extends Component {
         projects: [
             {
                 id: 1,
-                cls_slide_1: "slide-rainy",
+                cls_slide: "slide-rainy",
                 cls_slide_2: "slide-rainy-2",
                 cls_name: "rainyDay",
                 cls_h3:"h3-rainy",
                 name: "Rainy",
                 name_other_half: "Day",
+                cls_name_half:"b-rainy",
                 cls_mark_name: "mark-tech-rainy",
                 tech: "React.js - Bootstrap -Javascript - ",
                 tech_other_half: "Firebase",
                 cls_card: "card-rainy",
-                cls_svg_1: "svg-project-rainy",
+                svg:ready,
+                svg_2:readyTwo,
+                alt_svg:"Anime kimetsu no yaiba(Demon Slayer) background black",
+                alt_svg2:"Anime Kimetsu no yaiba(Demon Slayer) background yellow",
+                cls_svg: "svg-project-rainy",
                 cls_svg_2: "svg-project-rainy-2"
             },
             {
                 id: 2,
-                cls_slide_1: "slide-figur",
+                cls_slide: "slide-figur",
                 cls_slide_2: "slide-figur-2",
                 cls_name: "figurToys",
                 cls_h3:"h3-figur",
                 name: "FigurToys",
+                cls_mark_name: "mark-tech-figur",
                 tech: "Wordpress - Astra - ",
                 tech_other_half: "WooCommerce",
                 cls_card: "card-figur",
-                cls_svg_1: "svg-project-figur",
+                svg:sephi,
+                svg_2:epaule,
+                svg_3:cloud,
+                alt_svg:"Sephiroth",
+                alt_svg2:"Épaulière sephiroth color pink",
+                alt_svg3:"Cloud Strife de dos",
+                cls_svg: "svg-project-figur",
                 cls_svg_2: "svg-project-figur-2",
                 cls_svg_3: "svg-project-figur-3"
             },
             {
                 id: 3,
-                cls_slide_1: "slide-folio",
+                cls_slide: "slide-folio",
                 cls_slide_2: "slide-folio-2",
                 cls_name: "folio",
                 cls_h3:"h3-folio",
                 name: "Portfolio 2020",
+                cls_mark_name: "mark-tech-folio",
                 tech: "Bootstrap - Javascript  - Jquery - ",
                 tech_other_half: "Php",
                 cls_card: "card-folio",
-                cls_svg_1: "svg-project-folio"
+                svg:marvin,
+                alt_svg:"Atohoun marvin auteur du site (moi en gros)",
+                cls_svg: "svg-project-folio"
             },
             {
                 id: 4,
@@ -54,10 +76,13 @@ export class Main extends Component {
                 cls_name: "acacia",
                 cls_h3:"h3-acacia",
                 name: "Acacia",
+                cls_mark_name: "mark-tech-acacia",
                 tech: "Mvc - Materialize - Javascript  - Jquery - ",
                 tech_other_half: "Php",
                 cls_card: "card-acacia",
-                cls_svg_1: "svg-project-acacia"
+                svg:cuisto,
+                alt_svg:"chef cuistot cartoon",
+                cls_svg: "svg-project-acacia"
             }
         ]
     }
@@ -78,23 +103,29 @@ export class Main extends Component {
                     {this.state.projects.map(projet => (
                         <Project 
                             key={projet.id}
-                            cls_slide_1={projet.cls_slide_1}
+                            project_id={projet.id}
+                            cls_slide={projet.cls_slide}
                             cls_slide_2={projet.cls_slide_2}
                             cls_name={projet.cls_name}
                             cls_h3={projet.cls_h3}
                             name={projet.name}
                             name_other_half={projet.name_other_half}
+                            cls_name_half={projet.cls_name_half}
                             cls_mark_name={projet.cls_mark_name}
                             tech={projet.tech}
                             tech_other_half={projet.tech_other_half}
                             cls_card={projet.cls_card}
-                            cls_svg_1={projet.cls_svg_1}
+                            svg={projet.svg}
+                            svg_2={projet.svg_2}
+                            svg_3={projet.svg_3}
+                            alt_svg={projet.alt_svg}
+                            alt_svg_2={projet.alt_svg}
+                            alt_svg_3={projet.alt_svg}
+                            cls_svg={projet.cls_svg}
                             cls_svg_2={projet.cls_svg_2}
                             cls_svg_3={projet.cls_svg_3}
                         />
                         ))}
-                    
-
                 </main>
                 
             </Fragment>
--- src/home/projects/Project.js
+++ src/home/projects/Project.js
@@ -1,23 +1,40 @@
 import React, { Component, Fragment } from 'react';
-import ready from '../../assets/svg-projects/ready.svg';
-import readyTwo from '../../assets/svg-projects/readyTwo.svg';
 import './projects.scss';
 
 export class Project extends Component {
     render() {
         return (
             <Fragment>
-                <div className="slide-project-1 slide-rainy"></div>
-                <div className="slide-project-2 slide-rainy-2"></div>
-                <section className="projet rainyDay">
+                <div className={`slide-project ${this.props.cls_slide}`}></div>
+                <div className={`slide-project-2 ${this.props.cls_slide_2}`}></div>
+                <section className={`projet ${this.props.cls_name}`}>
                     <div className="col-preview">
-                        <h3 className={this.props.cls_h3}>Rainy<b className="b-rainy">Day</b></h3>
-                        <em>React.js - Bootstrap - Javascript -<mark className="mark-tech-rainy"> Firebase</mark></em>
-                        <div className="card-project card-rainy"></div>
+                        <h3 className={this.props.cls_h3}>
+                            {this.props.name}
+                                {this.props.project_id === 1 ? 
+                                    <b className={this.props.cls_name_half}>{this.props.name_other_half}</b>
+                                     : 
+                                     null
+                                }
+                        </h3>
+                        <em>
+                            {this.props.tech}
+                            <mark className={this.props.cls_mark_name}>{this.props.tech_other_half}</mark>
+                        </em>
+                        <div className={`card-project ${this.props.cls_card}`}></div>
                     </div>
                     <div id="#" className="col-mb">
-                        <img src={ready} alt="" className="svg-project-rainy"/>
-                        <img src={readyTwo} alt="" className="svg-project-rainy-2"/>
+                        <img src={this.props.svg} alt="" className={this.props.cls_svg}/>
+                        {this.props.project_id === 1 || 2 ? 
+                            <img src={this.props.svg_2} alt="" className={this.props.cls_svg_2}/>
+                            :
+                            null
+                        }
+                        {this.props.project_id === 2 ? 
+                            <img src={this.props.svg_3} alt="" className={this.props.cls_svg_3}/>
+                            :
+                            null
+                        }
                     </div>
                 </section>                
             </Fragment>
