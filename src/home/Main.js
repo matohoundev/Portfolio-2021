e50b31d6f2b31c8f536c85c2bdad72dfb5b39d5e
Nooxunath <mateksonic@gmail.com>
2020-12-23 18:27:34 +0100

    animation des background linear en cours

--- src/home/Main.js
+++ src/home/Main.js
@@ -99,12 +99,14 @@ export class Main extends Component {
     render() {
         return (
             <Fragment>
+                <div className="slide-Main"></div>
                 <header className="head-Main">
-                    <h1>ATOHOUN MARVIN</h1>
-                    <h2>Front-end Developper</h2>
+                        <h1>ATOHOUN MARVIN</h1>
+                        <h2>Front-end Developper</h2>
                 </header>
                 
                 <main>
+                    <div className="slide-about"></div>
                     <section className="about">
                         <p>Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services. Je réalise des sites internet avec le souci du détail et un rendu ﬁdèle au design.</p>
                     </section>
