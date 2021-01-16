a0ddd0cba3e2c45d712b54cde034b75e41fdf1a6
Nooxunath <mateksonic@gmail.com>
2021-01-16 20:17:26 +0100

    page 404 partiel

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { useEffect } from 'react';
 import ScrollToTop from './scrollRestoration/scrollToTop';
 import Aos from "aos";
 import "aos/dist/aos.css";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Navbar from './starter/navbar/NavBar';
 import Main from './home/Main';
 import Mission from './mission/Mission';
 import Contact from './home/contact/Contact';
+import NotFound from './home/404/Notfound';
 
 
 function App() {
@@ -25,6 +26,8 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route path="/mission" component={Mission} />
         <Route path="/contact" component={Contact} />
+        <Route path="/404" component={NotFound} />
+        <Redirect to="/404" />
       </Switch>
 
     </Router>
--- src/home/projects/Project.js
+++ src/home/projects/Project.js
@@ -1,89 +1,85 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 import './projects.scss';
 
-export class Project extends Component {
+const Project = props => {
 
-    state = {
-            missions: [
-                {
-                    id: this.props.project_id,
-                    name: this.props.name,
-                    name_other_half: this.props.name_other_half,
-                    cls_h3: this.props.cls_h3,
-                    cls_name_half: this.props.cls_name_half,
-                    tech: this.props.tech + this.props.tech_other_half,
-                    cls_head_img: this.props.cls_head_img,
-                    citation:this.props.citation,
-                    synopsis: this.props.synopsis,
-                    cls_half_img: this.props.cls_half_img,
-                    mission: this.props.mission,
-                    cls_img: this.props.cls_img,
-                }
-            ]
-        }
+        const [missions] = useState ([
+            {
+                id: props.project_id,
+                name: props.name,
+                name_other_half: props.name_other_half,
+                cls_h3: props.cls_h3,
+                cls_name_half: props.cls_name_half,
+                tech: props.tech + props.tech_other_half,
+                cls_head_img: props.cls_head_img,
+                citation: props.citation,
+                synopsis: props.synopsis,
+                cls_half_img: props.cls_half_img,
+                mission: props.mission,
+                cls_img: props.cls_img,
+            }
+        ])
 
-    render() {
+    let min_width = 844;
+    let match = useRouteMatch();
 
-        let min_width = 844;
+    return (
+        <div id="project" className="section">
 
-        return (
-            <div id="project" className="section">
+            <div className={`slide-project ${props.cls_slide}`}
+                data-aos="project-slide-f"
+            ></div>
 
-                <div className={`slide-project ${this.props.cls_slide}`} 
-                    data-aos="project-slide-f"
-                    ></div>
+            <section className={`projet ${props.cls_name} ${props.cls_slide_2}`}
+                data-aos="project-slide-b"
+            >
+                <div className="col-preview">
+                    <h3 className={props.cls_h3} data-aos="fade-left">
+                        {props.name}
+                        {min_width > document.body.clientWidth ?
+                            <b className={props.cls_name_half}>{props.name_other_half}</b>
+                            :
+                            <b className={props.cls_name_half_md}>{props.name_other_half}</b>
+                        }
+                    </h3>
+                    <em>
+                        {props.tech}
+                        <mark className={props.cls_mark_name}>{props.tech_other_half}</mark>
+                    </em>
 
-                <section className={`projet ${this.props.cls_name} ${this.props.cls_slide_2}`}
-                    data-aos="project-slide-b"
-                >
-                    <div className="col-preview">
-                        <h3 className={this.props.cls_h3} data-aos="fade-left">
-                            {this.props.name}
-                                {min_width > document.body.clientWidth ? 
-                                    <b className={this.props.cls_name_half}>{this.props.name_other_half}</b>
-                                     : 
-                                    <b className={this.props.cls_name_half_md}>{this.props.name_other_half}</b>
-                                }
-                        </h3>
-                        <em>
-                            {this.props.tech}
-                            <mark className={this.props.cls_mark_name}>{this.props.tech_other_half}</mark>
-                        </em>
-
-                        {this.state.missions.map(mission => (
-                            <Link
-                            key={this.props.project_id}
+                    {missions.map(mission => (
+                        <Link
+                            key={props.project_id}
                             to={{
-                                pathname: "/mission",
+                                pathname: `${match.url}mission/${props.name.toLowerCase() + props.name_other_half.toLowerCase()}`,
                                 state: {
                                     mission
                                 }
                             }}>
-                                <div className={`card-project ${this.props.cls_card}`}></div>
-                            </Link>
-                         ))}
+                            <div className={`card-project ${props.cls_card}`}></div>
+                        </Link>
+                        ))}
 
-                    </div>
+                </div>
 
-                    <div id="#" className="col-mb">
-                        <img src={this.props.svg} alt={this.props.alt_svg} className={this.props.cls_svg} data-aos="img-slide-left"/>
-                        {this.props.project_id === 1 || 2 ? 
-                            <img src={this.props.svg_2} alt={this.props.alt_svg_2} className={this.props.cls_svg_2} data-aos="img-slide-right"/>
-                            :
-                            null
-                        }
-                        {this.props.project_id === 2 ? 
-                            <img src={this.props.svg_3} alt={this.props.alt_svg_3} className={this.props.cls_svg_3} data-aos="img-slide-up"/>
-                            :
-                            null
-                        }
-                    </div>
-                    
-                </section>                
-            </div>
-        )
-    }
+                <div id="#" className="col-mb">
+                    <img src={props.svg} alt={props.alt_svg} className={props.cls_svg} data-aos="img-slide-left" />
+                    {props.project_id === 1 || 2 ?
+                        <img src={props.svg_2} alt={props.alt_svg_2} className={props.cls_svg_2} data-aos="img-slide-right" />
+                        :
+                        null
+                    }
+                    {props.project_id === 2 ?
+                        <img src={props.svg_3} alt={props.alt_svg_3} className={props.cls_svg_3} data-aos="img-slide-up" />
+                        :
+                        null
+                    }
+                </div>
+
+            </section>
+        </div>
+    )
 }
 
 export default Project
